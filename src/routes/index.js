3e40391eff1d605f4bfa83a3166d3b646c22cbfb
惊梦 <1449432824@qq.com>
2022-07-23 22:54:10 +0800

    useRoutes路由表

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+
+export default [
+    {
+        path: "/about",
+        element: <About></About>
+    },
+    {
+        path: "/home",
+        element: <Home></Home>
+    },
+    {
+        path: "/",
+        element: <Navigate to='/about'></Navigate>
+    },
+]
