bd8927a2f5d2eb77a0eba7aeced73f888decb861
惊梦 <1449432824@qq.com>
2022-07-26 21:36:18 +0800

    嵌套路由和params参数传递

---
+++ 17_嵌套路由和params参数传递/src/pages/Home/Message/index.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+export default function Message() {
+    const [messages] = useState([
+        { id: "01", title: "message1", content: "嗨嗨嗨" },
+        { id: "02", title: "message2", content: "来啦" },
+        { id: "03", title: "message3", content: "！！！" }
+    ])
+    return (
+        <div>
+            <ul>
+                {
+                    messages.map(item => {
+                        return (
+                            <li key={item.id}>
+                                <Link to={`detail/${item.id}/${item.title}/${item.content}`}>{item.title}</Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <hr />
+            <Outlet></Outlet>
+        </div>
+    )
+}
